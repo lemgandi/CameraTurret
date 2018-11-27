30026e006532067912b07784a6f565e695a70e30
Charles Shapiro <charles.shapiro@tomshiro.org>
2018-11-27 15:52:03 -0500

    Trying to figure out how servo actually works.

--- CameraTurret.ino
+++ CameraTurret.ino
@@ -14,6 +14,8 @@
 #define HSERVOMAX 170
 #define VSERVOMIN 60
 #define VSERVOMAX 170
+#define HSERVOCENTER ((HSERVOMAX - HSERVOMIN) / 2)
+#define VSERVOCENTER ((VSERVOMAX - VSERVOMIN) / 2)
 
 Servo Hservo;
 Servo Vservo;
@@ -24,8 +26,8 @@ char CmdBuffer[CMDSTRSIZE];
 char *InCursor;
 typedef enum cmdSendingStates {None,Sending,Received};
 cmdSendingStates CmdSendingState;
-//                        0   1     2  3
-typedef enum directions {Left,Right,Up,Down};
+//                        0   1     2  3     4
+typedef enum directions {Left,Right,Up,Down,Center};
 
 
 typedef struct command {
@@ -84,9 +86,13 @@ command parseCommand(char *cmdbuf)
       retVal.theDirection=Up;
    else if (0 == strcasecmp(thetok,"DOWN"))
       retVal.theDirection=Down;
-
-   thetok=strtok(0," ");
-   retVal.distance=atoi(thetok);
+   else if (0 == strcasecmp(thetok,"CENTER"))
+      retVal.theDirection=Center;
+   if(retVal.theDirection != Center)
+   {
+      thetok=strtok(0," ");
+      retVal.distance=atoi(thetok);
+   }
    
    return retVal;
 }
@@ -105,14 +111,30 @@ void setup() {
 void moveServo(command whichWhere)
 {
    Servo *servoP;
+   static int currentHDistance;
+   static int currentVDistance;
+   int mappedDistance;
    
-   if((whichWhere.theDirection == Left) || (whichWhere.theDirection == Right))
+   if((whichWhere.theDirection == Right) || (whichWhere.theDirection == Left))
       servoP=&Hservo;
    else
       servoP=&Vservo;
       
    if((whichWhere.theDirection == Left) || (whichWhere.theDirection == Down))
       whichWhere.distance=-whichWhere.distance;
+   switch(whichWhere.theDirection) {
+      case Left:      
+      case Right:
+         currentHDistance += whichWhere.distance;
+	 break;
+      case Up:
+      case Down:
+         currentVDistance += whichWhere.distance;
+	 break;
+      // case Center:
+      default:
+         break;
+   }
 #ifdef CHS_DEBUG
    if (servoP == &Hservo)
       Serial.print("servo: Hservo ");
@@ -121,10 +143,30 @@ void moveServo(command whichWhere)
    Serial.print("theDirection: ");
    Serial.print(whichWhere.theDirection);
    Serial.print(" Distance: ");
-   Serial.println(whichWhere.distance);
+   Serial.print(whichWhere.distance);
+   Serial.print(" currentHDistance: ");
+   Serial.print(currentHDistance);
+   Serial.print(" currentVDistance: ");
+   Serial.print(currentVDistance);
+   Serial.println(" ");
 #endif
-   servoP->write(whichWhere.distance);
-   
+   if(whichWhere.theDirection != Center)
+      servoP->write(whichWhere.distance);
+   else
+   {
+#ifdef CHS_DEBUG
+   Serial.print("Writing HDistance: ");
+   Serial.print(-currentHDistance);
+   Serial.print(" VDistance: " );
+   Serial.print(-currentVDistance);
+   Serial.println(" ");
+#endif
+      Hservo.write(HSERVOCENTER);
+      Vservo.write(VSERVOCENTER);
+
+      currentHDistance = currentVDistance = 0;
+   }
+   delay(15);
 }
 /*
   Loop part of main
---
+++ README.md
@@ -0,0 +1,14 @@
+# Camera Turret Control 
+
+This is code to control a 2-servo camera turret with the Arduino from a serial interface.
+
+## Protocol
+
+``` ^DIRECTION 99/
+
+Where DIRECTION = { UP DOWN LEFT RIGHT }
+
+and 99 is an appropriate number of steps.  
+```
+
+
